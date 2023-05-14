2ebd2fe4f82753b313a7c378e77c702501c2cf5e
wermass <real432@rambler.ru>
2023-05-14 10:16:04 +0300

    второй комит

--- 09.05.2023/theory_def.py
+++ 09.05.2023/theory_def.py
@@ -9,6 +9,7 @@ def count_above_threshold(numbers: list, threshold: int):
 temperatures = [20, 25, 27, 30, 30, 28, 24]
 temperatures1 = [20, 25, 27, 30, 30, 28, 24]
 temperatures2 = [20, 25, 27, 30, 30, 28, 24]
+temperatures3 = [20, 25, 27, 30, 30, 28, 24]
 threshold = 25
 result = count_above_threshold(temperatures, threshold)
 print(f"Количество элементов выше порога ({threshold}):", result)  # Output: Количество элементов выше порога (25): 3
